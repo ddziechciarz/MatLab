def716ed355a2c291aa1f576cfd784c276bb1f1d
ddziechciarz <alawon12@gmail.com>
2022-10-11 12:43:40 +0200

    Poprawka Problem1_2

--- Problem1_2.m
+++ Problem1_2.m
@@ -4,6 +4,11 @@ echo on
 a=1;b=5;c=4;
 
 delta = b*b-4*a*c,
-sqrDelta = sqrt(delta),
-zeroPoint_1 = (-b-sqrDelta) / 2*a,
-zeroPoint_2 = (-b+sqrDelta) / 2*a,
+if delta < 0
+    disp 'brak pierwiastków';
+elseif delta == 0
+    zero_point = -b/(2*a),
+else
+    sqrDelta = sqrt(delta),
+    zeroPoint_1 = (-b-sqrDelta) / 2*a,
+    zeroPoint_2 = (-b+sqrDelta) / 2*a,end
